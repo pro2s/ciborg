bb89044191f09e822a31981b0cb9420ba394b63d
protasov.s.s@gmail.com <protasov.s.s@gmail.com@83a16ada-48b3-11de-a4f5-9d9bc3506571>
2010-12-30 22:18:56 +0000

    Output contracts

--- trunk/contracts/views.py
+++ trunk/contracts/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.template import Context, loader
+from django.http import HttpResponse
+from devices.models import Contract, Company, Delivery
+from django.forms.models import modelformset_factory
+from django.shortcuts import render_to_response
+from django.forms import ModelForm
+from django.conf import settings
+ABOUT = "Раздел управления договорами"
+
+def index(request):
+    contracts_list = Contract.objects.all()
+    t = loader.get_template('contracts/index.html')
+    c = Context({
+    'contracts_list': contracts_list,
+    'about': ABOUT,
+    'menu': settings.MENU, 'activetag':'contracts',
+                        })
+    return HttpResponse(t.render(c))
